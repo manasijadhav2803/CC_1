4fd46121bec10d33f023a2941798db19c5257292
manasijadhav2803 <163092205+manasijadhav2803@users.noreply.github.com>
2024-10-11 10:44:14 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-printf(\n"This C program is generated On Gith hub Platform"\n);
-printf(\n"This C program is executing in Google app engine "\n);
+printf("\n\nThis C program is generated On Gith hub Platform\n\n");
+printf("\n\nThis C program is executing in Google app engine \n\n");
   return 0;
 }
